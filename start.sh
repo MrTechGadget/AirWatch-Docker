1fd5852d3a044a46581fece85f3fb2de716bdd6d
Joshua Clark <audioeng@gmail.com>
2019-02-11 23:40:21 -0500

    Update start.sh
    Added git pull on start.

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@ set -e
 
 # Build a configuration file from environment variables.
 /buildConfig.sh > aw-bulkdevices-script/AirWatchConfig.json
+# Pull latest script from GitHub
+git pull https://github.com/MrTechGadget/aw-bulkdevices-script.git
 
 pwsh -WorkingDirectory aw-bulkdevices-script
